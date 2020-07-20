09310a1e0fbc0c420ab3424a3e25d010779f9df7
AmarelleDiArgento <ewige.quel@gmail.com>
2020-07-19 21:25:25 -0500

    Arreglado :D

--- script.sh
+++ script.sh
@@ -1,37 +1,51 @@
 #!/bin/bash
-sudo nano sudo nano /etc/pacman.conf 
+sudo nano /etc/pacman.conf 
 sudo pacman-mirrors -g
 git clone https://aur.archlinux.org/yay.git
-cd yay || makepkg -si || 
+cd yay 
+makepkg -si
+yay -Syu
 echo "empezando instalacion de paquetes"
-yay -S papirus-dark-kde-git papirus-maia-icon-theme-git papirus-icon-theme-git libreoffice-fresh-es  papirus-libreoffice-theme-git visual-studio-code-bin intellij-idea-community-edition spring-tool-suite firefox-i18n-es-mx firefox-developer-edition-i18n-es-mx chromiun latte-dock-git freerdp libvncserver remmina teamviewer openssh anydesk gimp spotify mysql-workbench franz 
+yay -S  papirus-dark-kde-git papirus-maia-icon-theme-git papirus-icon-theme-git visual-studio-code-bin intellij-idea-community-edition spring-tool-suite firefox-i18n-es-mx firefox-developer-edition-i18n-es-mx chromium latte-dock-git freerdp libvncserver remmina teamviewer openssh gimp docker docker-compose gnome-keyring android-studio
 
 echo "Permisos de usuario Docker:"
-sudo groupadd docker || sudo usermod -aG docker $USER
+sudo groupadd docker | sudo usermod -aG docker $USER
 echo "Servicio de Docker:"
-sudo systemctl enable docker || sudo systemctl start docker 
+sudo systemctl enable docker | sudo systemctl start docker 
 echo "Servicio de SSH:"
-sudo systemctl enable sshd.service || sudo systemctl start sshd.service
+sudo systemctl enable sshd.service | sudo systemctl start sshd.service
+echo "GIT registrar correo y usuario"
+git config --global user.name "AmarelleDiArgento"
+git config --global user.email "ewige.quel@gmail.com"
+
+ssh-keygen -t rsa -b 4096 -C "ewige.qual@gmail.com"
+echo "SSH registros :D"
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_rsa
+
 echo "Servicio de TeamViewer:"
-systemctl start teamviewerd || systemctl enable teamviewerd
+sudo systemctl enable teamviewerd.service | systemctl start teamviewerd.service
 
 echo "empezando instalacion NVM"
-wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.2/install.sh || bash 
-
-echo "NVM Node 10"
-nvm install 10
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 
 echo "NVM Node 12"
 nvm install 12
-echo "NVM Asignando default 10"
-nvm alias default 10 
 
-echo "NVM 10 Paquetes angular 7, nodemon, pm2" 
-nvm use 10 
-npm install -g @angular/cli@7 || npm install -g nodemon || npm install -g pm2
+echo "NVM Node 14"
+nvm install 14
+echo "NVM Asignando default 14"
+nvm alias default 14
 
-echo "NVM 12 Paquetes angular 8, nodemon, pm2" 
-nvm use 12 
-npm install -g @angular/cli@latest || npm install -g nodemon || npm install -g pm2
+echo "NVM 12 Paquetes angular 12 nodemon" 
+nvm use 12
+npm i -g nodemon @angular/cli@9 @ionic/cli firebase-tools
+
+echo "NVM 12 Paquetes angular 14, nodemon, pm2" 
+nvm use 14 
+npm i -g nodemon @angular/cli @ionic/cli firebase-tools
+
+
+code ~/.ssh/id_rsa.pub
 
 
