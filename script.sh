96f945eb01070b2173f3d7dca83ba856182f5ca3
AmarelleDiArgento <ewige.quel@gmail.com>
2020-07-23 11:38:27 -0500

    Agregados postman y office-electron

--- script.sh
+++ script.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
-sudo nano /etc/pacman.conf 
+sudo nano /etc/pacman.conf
 sudo pacman-mirrors -g
 git clone https://aur.archlinux.org/yay.git
-cd yay 
+cd yay
 makepkg -si
+bash
 yay -Syu
 echo "empezando instalacion de paquetes"
-yay -S  papirus-dark-kde-git papirus-maia-icon-theme-git papirus-icon-theme-git visual-studio-code-bin intellij-idea-community-edition spring-tool-suite firefox-i18n-es-mx firefox-developer-edition-i18n-es-mx chromium latte-dock-git freerdp libvncserver remmina teamviewer openssh gimp docker docker-compose gnome-keyring android-studio
+yay -S papirus-dark-kde-git papirus-maia-icon-theme-git papirus-icon-theme-git visual-studio-code-bin intellij-idea-community-edition spring-tool-suite firefox-i18n-es-mx firefox-developer-edition-i18n-es-mx chromium latte-dock-git freerdp libvncserver remmina teamviewer openssh gimp docker docker-compose gnome-keyring android-studio postman ms-office-online-nativefier
 
 echo "Permisos de usuario Docker:"
 sudo groupadd docker | sudo usermod -aG docker $USER
 echo "Servicio de Docker:"
-sudo systemctl enable docker | sudo systemctl start docker 
+sudo systemctl enable docker | sudo systemctl start docker
 echo "Servicio de SSH:"
 sudo systemctl enable sshd.service | sudo systemctl start sshd.service
 echo "GIT registrar correo y usuario"
@@ -27,7 +28,8 @@ echo "Servicio de TeamViewer:"
 sudo systemctl enable teamviewerd.service | systemctl start teamviewerd.service
 
 echo "empezando instalacion NVM"
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh
+bash
 
 echo "NVM Node 12"
 nvm install 12
@@ -37,15 +39,12 @@ nvm install 14
 echo "NVM Asignando default 14"
 nvm alias default 14
 
-echo "NVM 12 Paquetes angular 12 nodemon" 
+echo "NVM 12 Paquetes angular 12 nodemon"
 nvm use 12
 npm i -g nodemon @angular/cli@9 @ionic/cli firebase-tools
 
-echo "NVM 12 Paquetes angular 14, nodemon, pm2" 
-nvm use 14 
+echo "NVM 12 Paquetes angular 14, nodemon, pm2"
+nvm use 14
 npm i -g nodemon @angular/cli @ionic/cli firebase-tools
 
-
-code ~/.ssh/id_rsa.pub
-
-
+nano ~/.ssh/id_rsa.pub
